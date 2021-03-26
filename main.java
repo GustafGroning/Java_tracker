6b4ceadecdc48c096be662b300e7e0eb933c4cae
Gustaf Gröning <gustafgroning@gmail.com>
2021-03-26 11:46:23 +0100

    started work on first 2 methods

--- main.java
+++ main.java
@@ -1,5 +1,36 @@
-class QuickStart {
-    public static void main(String[] args) {
-        System.out.println("Hi! What would you like to do? \n");
+
+import java.util.Scanner;
+
+
+
+class myClass {
+    static void start_up() {
+      System.out.println("Hello! What would you like to do today? \n");
+      user_choice();
     }
+  
+    static void user_choice() {
+        Scanner inputScan = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Type \"task\" to start tracking your work \ntype \"stats\" to see your logged time");
+        String taskChoice = inputScan.next();
+        if (!taskChoice.equals("task") && !taskChoice.equals("stats"))  {
+            System.out.println("illegal input, please try again! \n");
+            user_choice();
+        }
+        if (taskChoice.equals("task")) {
+            System.out.println("you chose task!");
+        }
+        if (taskChoice.equals("stats")) {
+            System.out.println("you chose stats!");
+        }
+        
+    }
+
+    //user chooses between "task" and "stats"
+    
+    
+
+  public static void main(String[] args) {
+    start_up();
+   }
 }
