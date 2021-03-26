f72fc405c50458862425c678faec9fb358c6aca1
Gustaf Gröning <gustafgroning@gmail.com>
2021-03-26 13:43:20 +0100

    working on third and fourth methods.

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
-
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -11,19 +12,32 @@ class myClass {
   
     static void user_choice() {
         Scanner inputScan = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Type \"task\" to start tracking your work \ntype \"stats\" to see your logged time");
+        System.out.println("Type \"task\" to start tracking your work \ntype \"stats\" to see your logged time \n");
         String taskChoice = inputScan.next();
-        if (!taskChoice.equals("task") && !taskChoice.equals("stats"))  {
-            System.out.println("illegal input, please try again! \n");
-            user_choice();
-        }
+        //inputScan.close();
+        
+     
         if (taskChoice.equals("task")) {
             System.out.println("you chose task!");
+            choose_task();
         }
         if (taskChoice.equals("stats")) {
             System.out.println("you chose stats!");
-        }
+
+        }// "else" ran even if the condition for "if" was satesfied, removing it until i figure out why.
+            
         
+    }
+
+    static void choose_task() {
+        List<String> categories = new ArrayList<String>();
+        categories.add("work");
+        categories.add("fun");
+        categories.add("waste");
+        System.out.println(categories);
+
+
+        //creates the category list, let's user choose which task to track.
     }
 
     //user chooses between "task" and "stats"
